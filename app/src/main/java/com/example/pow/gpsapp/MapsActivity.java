a2fa7782a58cafb484808b2c2003b52bceb74433
Ryan Pow <powryan@ymail.com>
2016-05-24 14:25:37 +0800

    Contact list finally working, awaiting actions for contacts

--- app/src/main/java/com/example/pow/gpsapp/MapsActivity.java
+++ app/src/main/java/com/example/pow/gpsapp/MapsActivity.java
@@ -1,21 +1,31 @@
  package com.example.pow.gpsapp;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -24,14 +34,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.pow.gpsapp.SelectUserAdapter;
+import com.example.pow.gpsapp.SelectUser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
  public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+     ArrayList<SelectUser> selectUsers;
+     List<SelectUser> temp;
+     ListView listView;
+     Cursor phones, email;
+     ContentResolver resolver;
+     SearchView search;
+     SelectUserAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,49 +60,124 @@ import java.util.List;
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Button btnContact= (Button)findViewById(R.id.btnContact);
-        btnContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showSortPopup(MapsActivity.this,null);
-            }
-        });
+
     }
 
-     private void showSortPopup(final Activity context, Point p)
+     public void contactpopup()
      {
-         // Inflate the popup_layout.xml
-         RelativeLayout viewGroup = (RelativeLayout) context.findViewById(R.id.ContactLayout);
-         LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-         View layout = layoutInflater.inflate(R.layout.contactpopup, viewGroup);
+         setContentView(R.layout.contactpopup);
 
-         // Creating the PopupWindow
-         final PopupWindow popup = new PopupWindow(context);
-         popup.setContentView(layout);
-         popup.setWidth(RelativeLayout.LayoutParams.WRAP_CONTENT);
-         popup.setHeight(RelativeLayout.LayoutParams.WRAP_CONTENT);
-         popup.setFocusable(true);
+         selectUsers = new ArrayList<SelectUser>();
+         resolver = this.getContentResolver();
+         listView = (ListView) findViewById(R.id.contacts_list);
 
-         // Some offset to align the popup a bit to the left, and a bit down, relative to button's position.
-         int OFFSET_X = -20;
-         int OFFSET_Y = 95;
+         phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC");
+         LoadContact loadContact = new LoadContact();
+         loadContact.execute();
 
-         // Clear the default translucent background
-         popup.setBackgroundDrawable(new BitmapDrawable());
-         // Displaying the popup at the specified location, + offsets.
-         popup.showAtLocation(layout, Gravity.CENTER, OFFSET_X, OFFSET_Y);
+         search = (SearchView) findViewById(R.id.searchView);
 
-         Button Back = (Button) layout.findViewById(R.id.btnBack);
-         Back.setOnClickListener(new View.OnClickListener() {
+         //*** setOnQueryTextListener ***
+         search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
 
              @Override
-             public void onClick(View v) {
-                 popup.dismiss();
+             public boolean onQueryTextSubmit(String query) {
+                 // TODO Auto-generated method stub
+
+                 return false;
+             }
+
+             @Override
+             public boolean onQueryTextChange(String newText) {
+                 // TODO Auto-generated method stub
+                 adapter.filter(newText);
+                 return false;
              }
          });
-         // Getting a reference to Close button, and close the popup when clicked.
+     }
 
+     // Load data on background
+     class LoadContact extends AsyncTask<Void, Void, Void> {
+         @Override
+         protected void onPreExecute() {
+             super.onPreExecute();
 
+         }
+
+         @Override
+         protected Void doInBackground(Void... voids) {
+             // Get Contact list from Phone
+
+             if (phones != null) {
+                 Log.e("count", "" + phones.getCount());
+                 if (phones.getCount() == 0) {
+                     Toast.makeText(MapsActivity.this, "No contacts in your contact list.", Toast.LENGTH_LONG).show();
+                 }
+
+                 while (phones.moveToNext()) {
+                     String id = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID));
+                     String name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                     String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                     String EmailAddr = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA2));
+                     SelectUser selectUser = new SelectUser();
+                     selectUser.setName(name);
+                     selectUser.setPhone(phoneNumber);
+                     selectUser.setEmail(id);
+                     selectUser.setCheckedBox(false);
+                     selectUsers.add(selectUser);
+                 }
+             } else {
+                 Log.e("Cursor close 1", "----------------");
+             }
+             //phones.close();
+             return null;
+         }
+
+         @Override
+         protected void onPostExecute(Void aVoid) {
+             super.onPostExecute(aVoid);
+             adapter = new SelectUserAdapter(selectUsers, MapsActivity.this);
+             listView.setAdapter(adapter);
+
+             // Select item on listclick
+             listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                 @Override
+                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                     Log.e("search", "here---------------- listener");
+
+                     SelectUser data = selectUsers.get(i);
+                 }
+             });
+
+             listView.setFastScrollEnabled(true);
+         }
+     }
+
+     @Override
+     protected void onStop() {
+         super.onStop();
+         phones.close();
+     }
+
+     public void contact_map(View view)
+     {
+         setContentView(R.layout.activity_maps);
+         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                 .findFragmentById(R.id.map2);
+         mapFragment.getMapAsync(this);
+     }
+     public void onSwitch(View view)
+     {
+         if(view.getId() == R.id.btnBack)
+         {
+             setContentView(R.layout.contact_map);
+         }
+         if(view.getId() == R.id.btnContact)
+         {
+             setContentView(R.layout.contactpopup);
+             contactpopup();
+         }
      }
 
      public void onZoom(View view)
@@ -92,6 +187,17 @@ import java.util.List;
              mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
          }
          if(view.getId() == R.id.btnzoomout)
+         {
+             mMap.animateCamera(CameraUpdateFactory.zoomTo(8), 2000, null);
+         }
+     }
+     public void onZoom2(View view)
+     {
+         if(view.getId() == R.id.btnzoomin2)
+         {
+             mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+         }
+         if(view.getId() == R.id.btnzoomout2)
          {
              mMap.animateCamera(CameraUpdateFactory.zoomTo(8), 2000, null);
          }
@@ -116,8 +222,28 @@ import java.util.List;
             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         }
     }
+     public void onSearch2(View view)
+     {
+         EditText location_tf = (EditText)findViewById(R.id.txtAddress2);
+         String location = location_tf.getText().toString();
+         List<Address> addressList = null;
+         if(location != null || location.equals(""))
+         {
+             Geocoder geocoder = new Geocoder(this);
+             try {
+                 addressList = geocoder.getFromLocationName(location, 1);
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+             Address address = addressList.get(0);
+             LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
+             mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+             mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+         }
+     }
 
-    @Override
+
+     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMyLocationEnabled(true);
