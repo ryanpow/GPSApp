e7053e020bfc1763023893118107259ad2609ced
Ryan Pow <powryan@ymail.com>
2016-07-15 22:30:34 +0800

    Can register an account onto the server

--- app/src/main/java/com/example/pow/gpsapp/MapsActivity.java
+++ app/src/main/java/com/example/pow/gpsapp/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.pow.gpsapp;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -8,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteStatement;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -17,7 +19,7 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Build;
-import android.provider.ContactsContract;
+import android.os.StrictMode;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -29,13 +31,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.telephony.SmsManager;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -51,7 +52,10 @@ import com.google.android.gms.common.ConnectionResult;
 
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.List;
 
 
@@ -65,8 +69,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     ContentResolver resolver;
     SearchView search;
     private Location mLastLocation;
-    public static TextView mainLabel, txtSSID1, txtSSID2, txtSSID3, txtMAC1, txtMAC2, txtMAC3, txtlevel1, txtlevel2, txtlevel3;
-    static String txtLocation, txtWifi;
+    public static TextView mainLabel, txtSSID1, txtSSID2, txtSSID3, txtMAC1, txtMAC2, txtMAC3, txtlevel1, txtlevel2, txtlevel3,usernameText,passwordText,usernametxt,passwordtxt;
+    static String txtLocation, txtWifi, username, password, sqlStatement, getSQLUsername, getSQLPassword;
     public LocationManager mLocationManager;
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
 //     String wifiresult=IncomingSMSReceiver.wifiresult;
@@ -98,13 +102,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS}, 123);
         }
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.loginmenu);
+        login();
         requestPermission(Manifest.permission.ACCESS_FINE_LOCATION,
                 LOCATION_REQUEST_CODE);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-        mainLabel = (TextView) findViewById(R.id.mainLabel);
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
         int LOCATION_REFRESH_TIME = 0;
         int LOCATION_REFRESH_DISTANCE = 0;
@@ -157,6 +158,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
              System.out.println(txtWifi);
          }
      }
+    public void Register(){
+        usernametxt = (TextView) findViewById(R.id.usernametxt);
+        passwordtxt = (TextView) findViewById(R.id.passwordtxt);
+        Button btnRegister = (Button) findViewById(R.id.btnsaveregister);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlStatement = "insert into GPSAccount values(1111,'"+usernametxt.getText()+"','"+passwordtxt.getText()+"')";
+                new WriteDatabase().execute();
+            }
+        });
+    }
      private final android.location.LocationListener mLocationListener = new android.location.LocationListener() {
          @Override
          public void onLocationChanged(Location location) {
@@ -258,75 +270,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          client.disconnect();
      }
 
-     // Load data on background
-
-
-     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-         if (data != null) {
-             Uri uri = data.getData();
-             String number = "";
-             if (uri != null) {
-                 Cursor c = null;
-                 try {
-                     c = getContentResolver().query(uri, new String[]{
-                                     ContactsContract.CommonDataKinds.Phone.NUMBER,
-                                     ContactsContract.CommonDataKinds.Phone.TYPE},
-                             null, null, null);
-
-                     if (c != null && c.moveToFirst()) {
-                         number = c.getString(0);
-                         int type = c.getInt(1);
-                         showSelectedNumber(type, number);
-                     }
-
-                     Log.i("Send SMS", "");
-                     String message = "Request";
-                     TextView textview = (TextView) findViewById(R.id.txtNumber);
-                     textview.setText(number);
-                     try {
-                         SmsManager smsManager = SmsManager.getDefault();
-                         smsManager.sendTextMessage(number, null, message, null, null);
-                         Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
-                         back();
-                     } catch (Exception e) {
-                         Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
-                         e.printStackTrace();
-                     }
-                 } finally {
-                     if (c != null) {
-                         c.close();
-                     }
-                 }
-             }
-
-         }
-
-     }
-
-     public void showSelectedNumber(int type, String number) {
-         Toast.makeText(this, type + ": " + number, Toast.LENGTH_LONG).show();
-     }
-
-
-     public void back(){
-         setContentView(R.layout.activity_maps);
-         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                 .findFragmentById(R.id.map);
-         mapFragment.getMapAsync(this);
-     }
 
      public void onSwitch(View view) {
-         if (view.getId() == R.id.btnBack) {
-             setContentView(R.layout.activity_maps);
-             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                     .findFragmentById(R.id.map);
-             mapFragment.getMapAsync(this);
-         }
          if (view.getId() == R.id.btnBack2) {
-             setContentView(R.layout.activity_maps);
-             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                     .findFragmentById(R.id.map);
-             mapFragment.getMapAsync(this);
+             generatemap();
          }
          if (view.getId() == R.id.btnFriend) {
              setContentView(R.layout.friendlist);
@@ -339,26 +286,52 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          }
          if (view.getId() == R.id.btnBackLogin) {
              setContentView(R.layout.loginmenu);
+             login();
          }
          if (view.getId() == R.id.btnregister) {
              setContentView(R.layout.registermenu);
+             Register();
          }
          if (view.getId() == R.id.btnCode) {
              setContentView(R.layout.codemenu);
          }
          if (view.getId() == R.id.btnBack3) {
-             setContentView(R.layout.activity_maps);
-             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                     .findFragmentById(R.id.map);
-             mapFragment.getMapAsync(this);
+             generatemap();
          }
          if (view.getId() == R.id.btnBack4) {
-             setContentView(R.layout.activity_maps);
-             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                     .findFragmentById(R.id.map);
-             mapFragment.getMapAsync(this);
+             generatemap();
          }
      }
+    public void login(){
+        usernameText = (TextView) findViewById(R.id.usernameText);
+        passwordText = (TextView) findViewById(R.id.passwordText);
+        Button btnLogin = (Button) findViewById(R.id.btnlogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sqlStatement = "select * from GPSAccount where Username='"+usernameText.getText()+"' AND Password='"+passwordText.getText()+"'";
+                new ReadDatabase().execute();
+                try {
+                    Thread.sleep(2500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if(getSQLUsername==usernameText.getText()){
+                    if(getSQLPassword==passwordText.getText()){
+                        generatemap();
+                    }
+                }
+
+            }
+        });
+
+
+    }
+    public void generatemap(){
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
      public void onZoom(View view) {
          if (view.getId() == R.id.btnzoomin) {
@@ -387,7 +360,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
              mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
          }
      }
-
      protected void requestPermission(String permissionType, int
              requestCode) {
          int permission = ContextCompat.checkSelfPermission(this,
@@ -411,9 +383,102 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                      Toast.makeText(this, "Unable to show location - permission required", Toast.LENGTH_LONG).show();
                  }
                  return;
+
              }
          }
      }
+    private class WriteDatabase extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+            @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+            @Override
+        protected Void doInBackground(Void... arg0) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            String url = "jdbc:jtds:sqlserver://182.50.133.109:1433;DatabaseName=tps";
+            String driver = "net.sourceforge.jtds.jdbc.Driver";
+            String userName = "RyanPow";
+            String password = "password123";
+            // Declare the JDBC objects.
+            Connection con = null;
+            Statement stmt = null;
+            ResultSet rs = null;
+            try
+            {
+                // Establish the connection.
+                Class.forName(driver);
+                con = DriverManager.getConnection(url, userName, password);
+                // Create and execute an SQL statement that returns some data.
+                String SQL = sqlStatement;
+                stmt = con.createStatement();
+                stmt.executeQuery(SQL);
+
+                Log.w("My Activity", "SQL No Error");
+            }
+            catch(Exception ex)
+            {
+                Log.w("My Activity", "SQL Error: "+ ex.toString());
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+
+    }
+
+    private class ReadDatabase extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            String url = "jdbc:jtds:sqlserver://182.50.133.109:1433;DatabaseName=tps";
+            String driver = "net.sourceforge.jtds.jdbc.Driver";
+            String userName = "RyanPow";
+            String password = "password123";
+            // Declare the JDBC objects.
+            Connection con = null;
+            Statement stmt = null;
+            ResultSet rs = null;
+            try
+            {
+                // Establish the connection.
+                Class.forName(driver);
+                con = DriverManager.getConnection(url, userName, password);
+                // Create and execute an SQL statement that returns some data.
+                String SQL = sqlStatement;
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(SQL);
+
+                /* Iterate through the data in the result set and display it. */
+                while (rs.next()) {
+                    getSQLUsername= rs.getString("username") ;
+                    getSQLPassword=rs.getString("password");
+                }
+                Log.w("My Activity", "SQL No Error");
+            }
+            catch(Exception ex)
+            {
+                Log.w("My Activity", "SQL Error: "+ ex.toString());
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(Void result) {
+        }
+
+    }
 
      @Override
      public void onMapReady(GoogleMap googleMap) {
